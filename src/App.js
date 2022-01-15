e281ace291b5e5d2aae8f658b5bca4f9a6e48fba
adhish-bahl <adhishbahl0@gmail.com>
2022-01-16 00:41:26 +0530

    Cleaning code and adding react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+                <Route path="/login">
+                    <h1>Login Page</h1>
+                </Route>
+
+                <Route path="/">
+                    <h1>This is react App</h1>
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
