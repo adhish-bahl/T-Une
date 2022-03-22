3df13b2a6f34b0c8f8bd395485d4d18eea522e7b
adhish-bahl <adhishbahl0@gmail.com>
2022-03-23 00:01:58 +0530

    created trending tables

--- src/Header.js
+++ src/Header.js
@@ -7,15 +7,71 @@ import store from "./images/store.svg"
 import quiz from "./images/quizlogo.png"
 import trending from "./images/TrendingIcon.svg";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";  
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Header() {
+
+
+    // console.log(document.URL);
+    // console.log(document.URL.lastIndexOf("/"));
+    // console.log(url);
+    
 
   const [homeActive, setHomeActive] = useState(true);
   const [exploreActive, setExploreActive] = useState(false);
   const [storeActive, setStoreActive] = useState(false);
   const [quizActive, setQuizActive] = useState(false);
   const [trendingActive, setTrendingActive] = useState(false);
+
+//   Router.onUrlChange(url1 => {
+//     const url = document.URL.slice(document.URL.lastIndexOf("/")+1, document.URL.length);
+//    console.log(url1);
+// });
+
+  // const checkURL = () => {
+  //   const url = document.URL.slice(document.URL.lastIndexOf("/")+1, document.URL.length);
+  //   if(url == "home") {
+  //     setHomeActive(homeActive => !homeActive);
+  //     setExploreActive(exploreActive => false);
+  //     setStoreActive(storeActive => false);
+  //     setQuizActive(quizActive => false);
+  //     setTrendingActive(trendingActive => false);
+  //   }
+
+  //   else if(url = "explore") {
+  //     setHomeActive(homeActive => false);
+  //     setExploreActive(exploreActive => !exploreActive);
+  //     setStoreActive(storeActive => false);
+  //     setQuizActive(quizActive => false);
+  //     setTrendingActive(trendingActive => false);
+  //   }
+
+  //   else if (url == "store") {
+  //     setHomeActive(homeActive => false);
+  //     setExploreActive(exploreActive => false);
+  //     setStoreActive(storeActive => !storeActive);
+  //     setQuizActive(quizActive => false);
+  //     setTrendingActive(trendingActive => false);
+  //   }
+
+  //   else if(url == "quiz") {
+  //     setHomeActive(homeActive => false);
+  //     setExploreActive(exploreActive => false);
+  //     setStoreActive(storeActive => false);
+  //     setQuizActive(quizActive => !quizActive);
+  //     setTrendingActive(trendingActive => false);
+  //   }
+
+  //   else if (url == "trending") {
+  //     setHomeActive(homeActive => false);
+  //     setExploreActive(exploreActive => false);
+  //     setStoreActive(storeActive => false);
+  //     setQuizActive(quizActive => false);
+  //     setTrendingActive(trendingActive => !trendingActive);
+  //   }
+  // }
 
   const handleHome = () => {
     setHomeActive(homeActive => !homeActive);
@@ -62,6 +118,11 @@ function Header() {
         </div>
         <div className="right">
           <ul>
+			  {/* <li><Link to="/home"><img className={` ${homeActive ? 'icon active' : 'icon'}`}  src={home} alt="HomeIcon" /></Link></li>
+			  <li><Link to="/explore"><img className={` ${exploreActive ? 'icon active' : 'icon'}`} src={explore} alt="ExploreIcon" /></Link></li>
+			  <li><Link to="/store"><img className={` ${storeActive ? 'icon active' : 'icon'}`} src={store} alt="StoreIcon" /></Link></li>
+			  <li><Link to="/quiz"><img className={` ${quizActive ? 'icon quizIcon active' : 'icon quizIcon'}`}  src={quiz} alt="QuizIcon" /></Link></li>
+			  <li><Link to="/trending"><img className={` ${trendingActive ? 'icon active' : 'icon'}`}  src={trending} alt="TrendingIcon" /></Link></li> */}
 			  <li><Link to="/home"><img className={` ${homeActive ? 'icon active' : 'icon'}`} onClick={handleHome} src={home} alt="HomeIcon" /></Link></li>
 			  <li><Link to="/explore"><img className={` ${exploreActive ? 'icon active' : 'icon'}`} onClick={handleExplore} src={explore} alt="ExploreIcon" /></Link></li>
 			  <li><Link to="/store"><img className={` ${storeActive ? 'icon active' : 'icon'}`} onClick={handleStore} src={store} alt="StoreIcon" /></Link></li>
--- src/Trending.js
+++ src/Trending.js
@@ -33,6 +33,65 @@ function Trending() {
   return (
     <div className='trendingContainer'>
         <div className="heading">Trending</div>
+        <div className="section">
+            <table border="1">
+                <tr>
+                    <th>S. No.</th>
+                    <th>Track Name</th>
+                    <th>Artist</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Track 1</td>
+                    <td>Artist 1</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Track 2</td>
+                    <td>Artist 2</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Track 3</td>
+                    <td>Artist 3</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Track 4</td>
+                    <td>Artist 4</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Track 5</td>
+                    <td>Artist 5</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Track 6</td>
+                    <td>Artist 6</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Track 7</td>
+                    <td>Artist 7</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>Track 8</td>
+                    <td>Artist 8</td>
+                </tr>
+                <tr>
+                    <td>9</td>
+                    <td>Track 9</td>
+                    <td>Artist 9</td>
+                </tr>
+                <tr>
+                    <td>10</td>
+                    <td>Track 10</td>
+                    <td>Artist 10</td>
+                </tr>
+            </table>
+        </div>
     </div>
   )
 }
