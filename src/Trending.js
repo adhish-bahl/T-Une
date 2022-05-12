803d08e360374705840c7b6c3ba083ef3fab7abd
adhish-bahl <adhishbahl0@gmail.com>
2022-05-12 09:14:30 +0530

    sorted trending page

--- src/Trending.js
+++ src/Trending.js
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Trending.css"
+import TrendingRow from './TrendingRow';
+import axiosbaseurl from "./axiosbaseurl";
 
 function Trending() {
 
+    var [data, setData] = useState({});
 
-    const show = () => {
-        const container = document.getElementById("trendingTable");
+    // useEffect(() => {
 
-        const mostTrending = ["Heat Waves", "Sacrifice", "Paris", "Black Space", "When I gone", "No lie", "It'll be okay", "High School", "Stay", "Unstoppable"];
-        const mostTrendingArtist = ["Heat Waves A", "Sacrifice A", "Paris A", "Black Space A", "When I gone A", "No lie A", "It'll be okay A", "High School A", "Stay A", "Unstoppable A"];
+    //     const xhttp = new XMLHttpRequest();
+    //     xhttp.open("POST", "http://localhost/DBMS%20Project/trending.php", false);
+    //     xhttp.onload = function(e) {
+    //         // console.table(this.responseText);
+    //         // setLogRes(logRes = incomingData.result);
+    //         // console.log(logRes);
+    //         const incomingData = JSON.parse(this.responseText);
+    //         console.table(incomingData);
+    //         setData(data = incomingData);
+    //     }
+    //     xhttp.send();
+    //     // console.log(data);
+    // }, []);
 
-        // dateDiv.innerHTML = "Top English Songs trending as on " + yo;
 
 
-        for(let i = 0; i < mostTrending.length; i++) {
-            const para = document.createElement("tr");
-            const node = document.createTextNode((i+1) +")  " + mostTrending[i]);
-            para.appendChild(node);
-            container.appendChild(para);
+
+    useEffect(() => {
+             async function fetchData() {
+        //  function fetchData() {
+            const request = await axiosbaseurl.get("Trending.php");
+            // const request = axiosbaseurl.get("Trending.php");
+            setData(data = request.data);
+            console.table(request.data);
+            return request;
         }
+        fetchData();
+    }, []);
+
+
+
+
+    // const show = () => {
+    //     const container = document.getElementById("trendingTable");
+
+    //     const mostTrending = ["Heat Waves", "Sacrifice", "Paris", "Black Space", "When I gone", "No lie", "It'll be okay", "High School", "Stay", "Unstoppable"];
+    //     const mostTrendingArtist = ["Heat Waves A", "Sacrifice A", "Paris A", "Black Space A", "When I gone A", "No lie A", "It'll be okay A", "High School A", "Stay A", "Unstoppable A"];
+
+    //     dateDiv.innerHTML = "Top English Songs trending as on " + yo;
+
+
+    //     for(let i = 0; i < mostTrending.length; i++) {
+    //         const para = document.createElement("tr");
+    //         const node = document.createTextNode((i+1) +")  " + mostTrending[i]);
+    //         para.appendChild(node);
+    //         container.appendChild(para);
+    //     }
         
-        for(let i = 0; i < mostTrending.length; i++) {
-            // console.log(mostTrending[i]);
-        }
-    }
+    //     for(let i = 0; i < mostTrending.length; i++) {
+    //         console.log(mostTrending[i]);
+    //     }
+    // }
 
 
   return (
@@ -33,63 +70,7 @@ function Trending() {
             <div className="trendingTable" id='trendingTable'>
 
             </div>
-            <table border="1">
-                <tr>
-                    <th>S. No.</th>
-                    <th>Track Name</th>
-                    <th>Artist</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Track 1</td>
-                    <td>Artist 1</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Track 2</td>
-                    <td>Artist 2</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Track 3</td>
-                    <td>Artist 3</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Track 4</td>
-                    <td>Artist 4</td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>Track 5</td>
-                    <td>Artist 5</td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>Track 6</td>
-                    <td>Artist 6</td>
-                </tr>
-                <tr>
-                    <td>7</td>
-                    <td>Track 7</td>
-                    <td>Artist 7</td>
-                </tr>
-                <tr>
-                    <td>8</td>
-                    <td>Track 8</td>
-                    <td>Artist 8</td>
-                </tr>
-                <tr>
-                    <td>9</td>
-                    <td>Track 9</td>
-                    <td>Artist 9</td>
-                </tr>
-                <tr>
-                    <td>10</td>
-                    <td>Track 10</td>
-                    <td>Artist 10</td>
-                </tr>
-            </table>
+            <TrendingRow data={data} />
         </div>
     </div>
   )
